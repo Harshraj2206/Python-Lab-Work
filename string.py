e78dd0e0cc5975780c891b7049968036256756f6
Harshraj2206 <hraj224488@gmail.com>
2026-09-17 10:19:48 +0530

    strings and its methods

---
+++ string.py
@@ -0,0 +1,33 @@
+name = "userplayer"
+# print(name[1:5]) # serp
+# print(name[-2]) # e
+# print(name[1:10:2]) # srl
+# print(name[::-1]) # replayresu
+
+
+# String Concatenation
+first_name = "Harsh"
+last_name = "Raj"
+print(first_name + " " + last_name) # Harsh Raj
+
+pattern = '''***
+***
+***
+'''
+print(pattern) 
+
+
+subject = "Python javascript Java C++"
+print("Python" in subject) # True
+print("Java" not in subject) # False
+
+st1 = "Python"
+st2 = "javascript"
+print(st1==st2) # False
+print(st1!=st2) # True
+print(st1>st2) # False
+
+
+
+
+
